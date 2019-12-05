120a4a30857400be3cdaa64a5f648f9fb2e2c35f
zionHuang <hjszion@gmail.com>
2019-12-05 00:24:38 -0800

    update

--- DP/62-different_paths.py
+++ DP/62-different_paths.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+#空间复杂度O(3n)
 def uniquePaths(m, n):
         opt = np.zeros([m, n])
         opt[0, :] = 1
@@ -8,6 +8,14 @@ def uniquePaths(m, n):
             for j in range(1, n):
                 opt[i][j] = opt[i][j-1] + opt[i-1][j]
         return opt[m-1, n-1]
-        
+
 uniquePaths(10,10)
-        
+
+#空间复杂度更低的解法O(n)
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        cur = [1] * n
+        for i in range(1, m):
+            for j in range(1, n):
+                cur[j] += cur[j-1]
+        return cur[-1]
