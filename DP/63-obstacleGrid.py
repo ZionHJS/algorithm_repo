fc2263868adfd1b4a0cfb818f0985f9b9bdfae29
zionHuang <hjszion@gmail.com>
2019-12-06 00:36:22 -0800

    update

--- DP/63-obstacleGrid.py
+++ DP/63-obstacleGrid.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def uniquePathsWithObstacles(self, obstacleGrid):
-        n = len(obstacleGrid)
-        m = len(obstacleGrid[0])
+        n = len(obstacleGrid)   #all-row list_length
+        m = len(obstacleGrid[0])  #single-row list_length
         if n == 1 or m == 1:
             for k in obstacleGrid[0]:
                 if k == 1:
@@ -9,20 +9,36 @@ class Solution(object):
             for l in obstacleGrid:
                 if l[0] == 1:
                     return 0
+        if obstacleGrid[0][0] == 1:
+            return 0
                     
         #initialize 1d grid           
         opt = [1]*n
+        for k in range(0, n):
+            if obstacleGrid[k][0] == 1:
+                for o in range(k, n):
+                    opt[o] = 0
         for i in range(1, m):
-            for j in range(1, n):
-                if obstacleGrid[i][j] == 1:
+            for j in range(0, n):
+                if obstacleGrid[j][i] == 1:
                     opt[j] = 0
-                elif opt[j-1] == 0:
-                    opt[j] = opt[j]
-                elif opt[j] == 0:
-                    opt[j] = opt[j-1]
-                elif opt[j] == 0 and opt[j-1] == 0:
-                    opt[j] = [0]
-                else:
+                elif opt[j-1] != None:
                     opt[j] += opt[j-1]
         return opt[-1]
 
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        for row in range(m):
+            for col in range(n):
+                if obstacleGrid[row][col] == 1:
+                    obstacleGrid[row][col] = 0
+                    continue
+                if row == 0 and col == 0:
+                    obstacleGrid[row][col] = 1
+                elif row == 0 or col == 0:
+                    obstacleGrid[row][col] = obstacleGrid[row][col-1] if row == 0 else obstacleGrid[row-1][col]
+                else:
+                    obstacleGrid[row][col] = obstacleGrid[row][col-1] + obstacleGrid[row-1][col]
+        return obstacleGrid[-1][-1]
