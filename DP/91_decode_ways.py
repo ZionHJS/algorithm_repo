7678c24583eb74fad2a08a1819711bb8e5aa0bec
zionHuang <hjszion@gmail.com>
2019-12-06 23:39:17 -0800

    update

--- DP/91_decode_ways.py
+++ DP/91_decode_ways.py
@@ -6,20 +6,26 @@ class Solution(object):
             return 0
 
         if len(s) >=2:
-            s_ = int(s[0:2])
-            if s_ <= 26:
+            s_ = s[0:2]
+            if int(s_) <= 26 and int(s_[1:2]) !=0:
                 opt[1] = 2
-            else:
-                pass
+            elif int(s_) <= 26 and int(s_[1:2]) == 0:
+                opt[1] = 1
 
         for i in range(2, s_len):
             if int(s[i]) == 0 and int(s[i-1]) == 0:
                 opt[i] = 0
-            elif int(s[i]) == 0:
+            elif int(s[i]) == 0 and int(s[i-1:i+1]) > 26:
+                opt[i] = 0
+            elif int(s[i]) == 0 and int(s[i-1:i+1]) <= 26:
                 opt[i] = opt[i-2]
-            elif int(s[i-1]) == 0:
+            elif int(s[i-1]) == 0 and int(s[i-2:i]) > 26:
+                opt[i] = 0
+            elif int(s[i-1]) == 0 and int(s[i-2:i]) <= 26:
                 opt[i] = opt[i-1]
-            else:
+            elif int(s[i-1:i+1]) > 26:
+                opt[i] = opt[i-1]
+            elif int(s[i-1:i+1]) <= 26:
                 opt[i] = opt[i-1] + opt[i-2]
         return opt[-1]
 
@@ -33,3 +39,4 @@ s = '1234'
 opt = [0]
 print(opt[-1])
 print(s[0])
+
