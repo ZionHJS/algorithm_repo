f59a29be7437ae1cf2d49c6e2c6a05da7b6341d1
zionHuang <hjszion@gmail.com>
2019-12-07 17:10:27 -0800

    update

--- DP/91_decode_ways.py
+++ DP/91_decode_ways.py
@@ -40,3 +40,37 @@ opt = [0]
 print(opt[-1])
 print(s[0])
 
+
+
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n=len(s)
+        if(not s or s[0]=="0"): #头位非零判断
+            return 0
+
+        dp=[0]*(n+1)
+        dp[0]=1
+        dp[1]=1
+        for i in range(1,n):
+            if(s[i]=="0"):
+                if(s[i-1]=="1" or s[i-1]=="2"):
+                    dp[i+1]=dp[i-1]
+                else:
+                    return 0
+            else:
+                if(s[i-1]=="1" or (s[i-1]=="2" and "1"<=s[i]<="6")):
+                    dp[i+1]=dp[i]+dp[i-1]
+                else:
+                    dp[i+1]=dp[i]
+        return dp[-1]
+
+
+
+
+class Solution:
+    def numDecodings(self, s)
+        pp, p = 1, int(s[0] != '0')
+        for i in range(1, len(s)):
+            pp, p = p, pp * (9 < int(s[i-1:i+1]) <= 26) + p * (int(s[i]) > 0)
+        return p
