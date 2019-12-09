01a2fd5f14f69448bf99532d5a2df9f4f7b4b134
zionHuang <hjszion@gmail.com>
2019-12-08 20:35:43 -0800

    update

--- JiuZhang/test.py
+++ JiuZhang/test.py
@@ -2,12 +2,10 @@ nums = (1, 5, 2, -4)
 nums.append(3)
 print(nums)
 
-
 nums = [1,5,2,-5,2,4,-6,8]
 nums = (1,5,2,-5,2,4,-6,8)
 for (i, score) in enumerate(nums):
     print(i, score)
-
 
 aList = [123, 'Google', 'Runoob', 'Taobao', 'Facebook',223, 12, 2, 1, 0, 'zion']
 aList.sort()
@@ -18,3 +16,5 @@ print(aList)
 #list生成器
 result = [i for i in range(101) if i % 5 == 0]
 print(result)
+
+
