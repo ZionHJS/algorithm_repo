5f0a6f745e8260a6249eaf4b45917a1c08b71f0b
zionHuang <hjszion@gmail.com>
2019-12-08 19:17:04 -0800

    update

--- JiuZhang/test.py
+++ JiuZhang/test.py
@@ -1,3 +1,17 @@
 nums = (1, 5, 2, -4)
 nums.append(3)
 print(nums)
+
+
+nums = [1,5,2,-5,2,4,-6,8]
+nums = (1,5,2,-5,2,4,-6,8)
+for (i, score) in enumerate(nums):
+    print(i, score)
+
+
+aList = [123, 'Google', 'Runoob', 'Taobao', 'Facebook',223, 12, 2, 1, 0, 'zion']
+aList.sort()
+print(aList)
+aList.reverse()
+print(aList)
+
