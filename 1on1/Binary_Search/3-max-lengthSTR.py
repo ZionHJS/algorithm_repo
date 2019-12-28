a975e320a5d23d980b18eac71c627ff5da9d89b4
zionHuang <hjszion@gmail.com>
2019-12-28 11:59:28 -0800

    updated 3

--- 1on1/Binary_Search/3-max-lengthSTR.py
+++ 1on1/Binary_Search/3-max-lengthSTR.py
@@ -5,18 +5,17 @@ class Solution:
         alph_counts = [0 for _ in range(256)]
         count = 0
         max_len = 0
-
         i, j = 0, 0
 
         while j < len(s):
             char = ord(s[j])
             alph_counts[char] += 1
-            if alph_counts[char] == 1:
+            if alph_counts[char] > 1:
                 count += 1
-            while i < len(s) and count > 2:
+            while i < len(s) and count >= 1:
                 char = ord(s[i])
                 alph_counts[char] -= 1
-                if alph_counts[char] == 0:
+                if alph_counts[char] == 1:
                     count -= 1
                 i += 1
             max_len = max(max_len, j-i+1)
