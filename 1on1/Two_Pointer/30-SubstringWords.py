f80208a852d82d001abe89a4711ab3c8d0b6ab5d
zionHuang <hjszion@gmail.com>
2019-12-30 01:05:26 -0800

    update

--- 1on1/Two_Pointer/30-SubstringWords.py
+++ 1on1/Two_Pointer/30-SubstringWords.py
@@ -1,37 +1,39 @@
+import copy
+
+
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        if not words:
-            return 0
-        elif not s:
-            return -1
-
-        single_len = len(words[0])
-        words_total_len = single_len*len(words)
+        if len(words) > 0:
+            single_len = len(words[0])
+            words_total_len = single_len*len(words)
         s_len = len(s)
-
-        words_start = []
-        for item in words:
-            words_start.append(item[0])
+        if not words:
+            return []
+        elif not s or s_len < words_total_len:
+            return []
 
         res = []
-
         i, j = 0, 0
         while i <= s_len - words_total_len:
-            if s[i] in words_start:
-                tmp_words = words
-                while i <= s_len - single_len:
-                    j = i + single_len
-                    tmp_s = s[i:j]
-                    if tmp_s in tmp_words:
-                        tmp_words.remove(tmp_s)
-                    else:
-                        break
+            for item in words:
+                if s[i] == item[0]:
+                    tmp_i = i
+                    tmp_words = copy.deepcopy(words)
+                    while len(tmp_words) > 0:
+                        j = tmp_i + single_len
+                        tmp_s = s[tmp_i:j]
+                        if tmp_s in tmp_words:
+                            tmp_words.remove(tmp_s)
+                        else:
+                            break
 
-                    if not tmp_words:
-                        res.append(j-words_total_len)
-                        break
-                    else:
-                        i += single_len
+                        if len(tmp_words) == 0:
+                            res.append(j-words_total_len)
+                        else:
+                            tmp_i += single_len
+                    break
             i += 1
 
         return res
+
+# 尼玛这么牛逼的算法居然超时
