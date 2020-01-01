09d1752a01478f2f694f05b093021821136b8846
zionHuang <hjszion@gmail.com>
2020-01-01 15:15:18 -0800

    update 1130

--- 1on1/BinaryTree/1130-leafsum.py
+++ 1on1/BinaryTree/1130-leafsum.py
@@ -7,7 +7,7 @@ class Solution:
             return None
 
         for i in range(0, len(arr) - 1):  # when len > = 2 and len must >= 2
-            root = TreeNode(max(arr[0:i+1]) + max(arr[i+1:]))
+            root = TreeNode(max(arr[0:i+1])*max(arr[i+1:]))
             root.left = self.nodefromlist(arr[0:i+1])  # len must >= 1
             root.right = self.nodefromlist(arr[i+1:])  # len must >= 1
 
@@ -16,29 +16,30 @@ class Solution:
         return min(self.min_res)
 
     def nodefromlist(self, list):
+        print(list)
         if len(list) == 0:
             return None
         elif len(list) == 1:
             return TreeNode(list[0])
-
-        for j in range(0, len(list) - 1):  # len must >= 2
-            root = TreeNode(max(list[0:j+1]) + max(list[j+1:]))
-            root.left = self.nodefromlist(max(list[0:j+1]))
-            root.right = self.nodefromlist(max(list[j+1:]))
+        else:
+            # when len(list) >= 2
+            for j in range(0, len(list) - 1):
+                root = TreeNode(max(list[0:j+1])*max(list[j+1:]))
+                root.left = self.nodefromlist(list[0:j+1])
+                root.right = self.nodefromlist(list[j+1:])
 
     def non_leaf_sum(self, root, nonleafsum):
         if not root:
-            return 0
-        if not root.left and not root.right:
+            return nonleafsum
+        if not root.left and not root.right:  # end condition
             return nonleafsum
         else:
-            nonleafsum += root.val
+            non_leaf_sum_left = self.non_leaf_sum(
+                root.left, nonleafsum + root.val)
+            non_leaf_sum_right = self.non_leaf_sum(
+                root.right, nonleafsum + root.val)
 
-        non_leaf_sum_left = leaf_sum(root.left, nonleafsum)
-        non_leaf_sum_right = leaf_sum(root.right, nonleafsum)
-
-        non_leaf_sum = non_leaf_sum_left + non_leaf_sum_right
-        return non_leaf_sum
+            return non_leaf_sum_left + non_leaf_sum_right
 
 
 list_a = [0, 1, 2, 3, 4, 5, 6]
