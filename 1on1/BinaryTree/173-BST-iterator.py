54416e62c707e166c1ba878dc40e1132f8f18726
zionHuang <hjszion@gmail.com>
2020-01-02 00:12:14 -0800

    explore 173

--- 1on1/BinaryTree/173-BST-iterator.py
+++ 1on1/BinaryTree/173-BST-iterator.py
@@ -11,32 +11,46 @@ class BSTIterator:
         self.root = root
         self.cur_root = root
         self.pre_root = root
-        self.res = 0
+        self.res = root.val
 
     def next(self) -> int:
-        if not self.cur_root:
-            self.cur_root = self.root.right
-            self.res = self.root.val
-            self.root = None
-            return self.res
+        if not self.cur_root.val:   #
+            self.cur_root = self.root
+            self.next()
 
         if self.cur_root.left and self.cur_root.right:
             self.cur_root = self.cur_root.left
             self.pre_root = self.cur_root
             self.next()
-        elif not self.cur_root.left and self.cur_root.right:
-            self.cur_root = self.cur_root.right
-            self.res = self.cur_root.val
-            self.cur_root.val = None
-            return self.res
+        elif self.cur_root.left or self.cur_root.right:
+            self.pre_root = self.cur_root
+            if self.cur_root.left:
+                self.cur_root = self.cur_root.left
+            else:
+                self.cur_root = self.cur_root.right
+
+            if self.pre_root.val:
+                self.res = self.pre_root.val
+                self.pre_root.val = None  # right step
+                return self.res
+            else:
+                self.next()
         else:
-            self.res = self.cur_root.val
-            self.cur_root.val = None
-            self.cur_root = self.pre_root
-            return self.res
+            if self.pre_root.left:  # cut left first and then cut right
+                self.pre_root.left = None
+            else:
+                self.pre_root.right = None
+
+            if not self.cur_root.val:
+                self.cur_root = self.pre_root
+                self.next()
+            else:
+                self.res = self.cur_root.val
+                self.cur_root = self.pre_root
+                return self.res
 
     def hasNext(self) -> bool:
-        if self.next():
-            return True
-        else:
+        if not self.next():
             return False
+        else:
+            return True
