b8422ab6929a5576bb91f594c4d1b49d7c7edfba
zionHuang <hjszion@gmail.com>
2020-01-03 15:21:35 -0800

    update973

--- 1on1/Data-Structure/973-k-closestpoints.py
+++ 1on1/Data-Structure/973-k-closestpoints.py
@@ -7,9 +7,19 @@ class Solution(object):
             return []
 
         dic = {}
+        heap = []
 
         for i in points:
             dis_i = self.get_dis(points[i])
+            if dis_i not in dic:
+                dic[dis_i] = []
+            dic[dis_i].append(points[i])  # zipper
+
+        for j in range(k):
+            for m in dic:
+                heapq.heappush(heap, m)
+
+        return heapq.heappop(heap)
 
     def get_dis(self, point):
         return sqrt(pow(point[0], 2) + pow(point[1], 2))
