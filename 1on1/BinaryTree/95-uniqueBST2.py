7bc53acff07caa7459256653ca91a2261a075563
zionHuang <hjszion@gmail.com>
2020-01-02 23:50:49 -0800

    update-95

--- 1on1/BinaryTree/95-uniqueBST2.py
+++ 1on1/BinaryTree/95-uniqueBST2.py
@@ -1,37 +1,36 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
 
-class Solution:
-    def __init__(self):
-        self.nlist = []
-        self.res = []
-
-    def generateTrees(self, n: int) -> List[TreeNode]:
+class Solution(object):
+    def generateTrees(self, n):
         if n < 1:
             return []
         if n == 1:
             return [TreeNode(1)]
         else:
-            self.nlist = [x for x in range(1, n+1)]
-            self.treeHelper(self.nlist)
+            self.nlist = [i for i in range(1, n+1)]
 
-            return self.res
+            return self.treeHelper(self.nlist)
 
     def treeHelper(self, list):
+        res = []
         if not list:
             return [None]
         if len(list) == 1:
             return [TreeNode(list[0])]
         else:
             for i in range(0, len(list)):  # index
-                for left in self.treeHelper(list[:i]):
-                    for right in self.treeHelper(list[i+1:]):
+                for l in self.treeHelper(list[:i]):
+                    for r in self.treeHelper(list[i+1:]):
                         root = TreeNode(list[i])
-                        root.left = left
-                        root.right = right
-                        self.res.append(root)
+                        root.left = l
+                        root.right = r
+
+                        res.append(root)
+
+            return res
