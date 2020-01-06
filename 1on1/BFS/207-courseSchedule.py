19338f8a343127ccd103ceadeee1e1fafaf918bb
zionHuang <hjszion@gmail.com>
2020-01-05 23:43:15 -0800

    update 207

--- 1on1/BFS/207-courseSchedule.py
+++ 1on1/BFS/207-courseSchedule.py
@@ -2,7 +2,7 @@ import queue
 
 
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+    def canFinish(self, num: int, pre: List[List[int]]) -> bool:
         if not num or not pre:
             return True
 
@@ -11,29 +11,29 @@ class Solution:
         for i in range(num):  # labbeld from 0 to n-1
             graph[i] = []
         for i in range(len(pre)):
-            graph[pre[i][1]].append(pre[i][0])  # post:[pres]
+            graph[pre[i][1]].append(pre[i][0])  # pre:[post]
 
         # map records each course's indegree number
         indegree_map = {}
         for i in range(num):
-            for pre in graph[i]:
-                if pre not in indegree_map:
-                    indegree_map[pre] = 1
+            for post in graph[i]:  # ??? at least it is a [], you can for in []
+                if post not in indegree_map:
+                    indegree_map[post] = 1
                 else:
-                    indegree_map[pre] += 1
+                    indegree_map[post] += 1
 
         # put course which's indegree = 0 into queue
         q = queue.Queue()
         for i in range(num):
-            if i not in indegree_map:
+            if i not in indegree_map:  # put the node into the queue starts with these without pres
                 q.put(i)
         count = 0
         while q.qsize():
             cur = q.get()
             count += 1
-            for pre in graph[cur]:
-                indegree_map[pre] -= 1
-                if indegree_map[pre] == 0:
-                    q.put(pre)
+            for post in graph[cur]:
+                indegree_map[post] -= 1
+                if indegree_map[post] == 0:
+                    q.put(post)
         # return Boolean
-        return count == num
+        return count == num  # did we counted all the courses???
