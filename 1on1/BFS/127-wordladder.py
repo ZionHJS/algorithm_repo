e2cc0ee9b975e6ce8e869cd084ee26b224f537af
zionHuang <hjszion@gmail.com>
2020-01-06 22:47:51 -0800

    updated 200

---
+++ 1on1/BFS/127-wordladder.py
@@ -0,0 +1,46 @@
+import queue
+
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if not wordList:
+            return 0
+        if beginWord == endWord:
+            return 1
+
+        word_list = set()
+        for word in wordList:
+            word_list.add(word)
+
+        # BFS starts here
+        dis = 1
+        q = queue.Queue()
+        s = set()  # to avoid repeat loop-bfs-
+        q.put(beginWord)
+        s.add(beginWord)
+        while q.qsize():
+            dis += 1
+            print(dis)
+            size = q.qsize()
+            for i in range(size):
+                word = q.get()
+                # all word from nextword dict
+                for next_word in self.get_nextword(word):
+                    if next_word not in word_list or next_word in s:
+                        continue
+                    if next_word == endWord:
+                        return dis
+                    q.put(next_word)
+                    s.add(next_word)
+        return 0
+
+    # get the next word
+    def get_nextword(self, word):
+        nextword_list = []
+        for i in range(len(word)):
+            left, right = word[:i], word[i+1:]
+            for char in 'abcdefghijklmnopqrstuvwxyz':
+                if word[i] == char:
+                    continue
+                nextword_list.append(left + char + right)
+        return nextword_list
---
+++ 1on1/BFS/200-NumberOfIsland.py
@@ -0,0 +1,32 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+
+        islands = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.bfs(grid, i, j)
+                    islands += 1
+        return islands
+
+    def bfs(self, grid, x, y):
+        queue = deque([(x, y)])
+        grid[x][y] = '0'
+        dx = [0, 0, 1, -1]
+        dy = [1, -1, 0, 0]
+        while queue:
+            x, y = queue.popleft()
+
+            for i in range(4):
+                next_x = x + dx[i]
+                next_y = y + dy[i]
+                if not self.is_valid(gird, next_x, next_y):
+                    continue
+                queue.append((next_x, next_y))
+                grid[next_x][next_y] = '0'
+
+    def is_valid(self, grid, x, y):
+        n, m = len(grid), len(grid[0])
+        return 0 <= x < n and 0 <= y < m and grid[x][y] == '1'
