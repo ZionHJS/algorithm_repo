930bcc62a47e2f0805a6c31fedac103f53d227e6
zionHuang <hjszion@gmail.com>
2020-01-19 13:24:38 -0800

    update

---
+++ 1on1/DFS+Recursion/39-CombinationSum.py
@@ -0,0 +1,29 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        if not candidates or not target:
+            return []
+
+        candidates.sort()
+        res = []
+        temp = []
+
+        self.dfs(res, temp, 0, candidates, target)
+        return res
+
+    def dfs(self, res, temp, index, candidates, target):
+        if target < 0:
+            return
+        if target == 0:
+            res.append(temp[:])
+            return
+
+        for i in range(index, len(candidates)):
+            if candidates[i] > target:
+                break
+
+            # if i > 0 and candidates[i] == candidates[i-1]:   #this part is checking the duplicates
+                # continue
+
+            temp.append(candidates[i])
+            self.dfs(res, temp, i, candidates, target - candidates[i])
+            temp.pop()
