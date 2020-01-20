6c0f1679ef9c9fd48d03fbbdc607fbe13306c413
zionHuang <hjszion@gmail.com>
2020-01-20 13:17:26 -0800

    update

--- 1on1/DFS+Recursion/847-VisitingNode.py
+++ 1on1/DFS+Recursion/847-VisitingNode.py
@@ -3,18 +3,24 @@ import queue
 
 class Solution:
     def shortestPathLength(self, graph: List[List[int]]) -> int:
-        if len(graph) == 1:
-            return len(graph[0])
+        if any(len(g) == 0 for g in graph):  # 如果有一个点没有边
+            return 0
 
-        graph_ = {}
-        count = 0
+        N = len(graph)  # length
 
-        for i in range(len(graph)):
-            graph_[i] = graph[i]
-
-        self.dfs(graph_)
-
-    def dfs(self, graph):
-        #q = queue.Queue()
-        #s = set()
-        for key in graph:
+        def bfs():
+            from collections import deque
+            Q = deque([(i, 0, {i}) for i in range(N)])
+            pruning = collections.defaultdict(lambda: float('inf'))
+            while Q:
+                i, d, seen = Q.popleft()
+                # pruning[tuple(sorted(seen))+(i,)]=d
+                if len(seen) == N:
+                    return d
+                for j in graph[i]:
+                    this_seen = seen | {j}
+                    this_key = tuple(sorted(this_seen))+(j,)
+                    if pruning[this_key] > d+1:
+                        pruning[this_key] = d+1
+                        Q.append((j, d+1, this_seen))
+        return bfs()
