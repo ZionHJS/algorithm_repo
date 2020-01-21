4fe714075bfb50b2a10ab053b12ea9ab23e410f5
zionHuang <hjszion@gmail.com>
2020-01-20 16:15:38 -0800

    update

--- 1on1/DFS+Recursion/547-friendcircle.py
+++ 1on1/DFS+Recursion/547-friendcircle.py
@@ -7,7 +7,9 @@ class Solution:
             return 0
 
         graph = {}
-        circle_count = 0
+        res = []
+        visited_s = set()
+        #circle_count = 0
 
         # build the graph
         for i in range(len(M)):
@@ -17,7 +19,15 @@ class Solution:
                     graph[i].append(j)
         # print(graph)
 
-    def bfs(self, graph, stu, count):
+        # loop the bfs   p => person
+        for p in range(len(M)):
+            #print('k:', k)
+            if p not in visited_s:
+                self.bfs(graph, p, res, visited_s)
+
+        return len(res)
+
+    def bfs(self, graph, stu, res, visited_s):
         q = queue.Queue()
         s = set()
         q.put(stu)
@@ -30,4 +40,28 @@ class Solution:
                     if friend not in s:
                         q.put(friend)
 
-        return count += 1
+        res.append(s)
+        for item in s:
+            if item not in visited_s:
+                visited_s.add(item)
+
+
+res = []
+s1 = set()
+s1.add(0)
+s1.add(1)
+s1.add(2)
+s1.add(3)
+res.append(s1)
+
+s2 = set()
+s2.add(4)
+s2.add(5)
+s2.add(6)
+s2.add(7)
+# res.append(s2)
+for item in s2:
+    print('item:', item)
+
+print('res is:', res)
+print('length res:', len(res))
