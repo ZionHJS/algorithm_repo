2c672caf1f5efd46e2b5d20d087a5c702d1b0521
ZionHJS <hjszion@gmail.com>
2020-02-15 17:02:05 -0800

    updated 1042

--- 1on1/Data-Structure/264-UglyNumbers.py
+++ 1on1/Data-Structure/264-UglyNumbers.py
@@ -31,3 +31,28 @@ class Solution:
                                 heapq.heappush(self.heap, -cur_multi*factor)
 
         return -self.heap[0]
+
+
+class Solution:
+    def isUgly(self, num: int) -> bool:
+        if num <= 0:
+            return False
+
+        # dfs
+        if num == 1:
+            return True
+
+        q, r = divmod(num, 2)  # q => 商, r => 余
+
+        if r == 0:
+            return self.isUgly(q)
+        else:
+            q, r = divmod(num, 3)
+            if r == 0:
+                return self.isUgly(q)
+            else:
+                q, r = divmod(num, 5)
+                if r == 0:
+                    return self.isUgly(q)
+                else:
+                    return False
--- 1on1/Data-Structure/test.py
+++ 1on1/Data-Structure/test.py
@@ -1,11 +1,1 @@
-
-char_dict = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 2, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0,
-             'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 1, 's': 0, 't': 1, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
-
-
-a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-for i in range(len(a)):
-    print("i:", i)
-    print("now_len(a):", len(a))
-    a.pop()
+print("divmod:", divmod(10, 3))
