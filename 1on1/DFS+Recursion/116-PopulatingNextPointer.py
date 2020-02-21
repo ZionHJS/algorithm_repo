e0c7f807810b6554648c92ca1fb566a909ff673b
ZionHJS <hjszion@gmail.com>
2020-02-20 22:42:42 -0800

    update

---
+++ 1on1/DFS+Recursion/116-PopulatingNextPointer.py
@@ -0,0 +1,40 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+import queue
+
+
+class Solution:
+    def __init__(self):
+        self.res = []
+
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return self.res
+        elif not root.left and not root.right:
+            return [root.val, None]
+
+        self.bfs(root)
+        return self.res
+
+    def bfs(self, root):
+        q = queue.Queue()
+        q.put(root)
+        q.put(None)
+
+        while q.qsize():
+            if q.qsize() > 0:
+                cur_node = q.get()
+                if cur_node != None:
+                    if cur_node.left:
+                        q.put(cur_node.left)
+                    if cur_node.right:
+                        q.put(cur_node.right)
+                    self.res.append(cur_node.val)
+            self.res.append(None)
