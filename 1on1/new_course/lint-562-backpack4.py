55f01ff0530fe0f78da66800a893173eb6a4c782
ZionHJS <hjszion@gmail.com>
2020-02-29 10:19:22 -0800

    update

--- 1on1/new_course/lint-562-backpack4.py
+++ 1on1/new_course/lint-562-backpack4.py
@@ -23,3 +23,39 @@ def backPackIV(self, nums, target):
     #print("dp:", dp)
 
     return dp[-1]
+
+
+# 563
+class Solution:
+    """
+    @param nums: an integer array and all positive numbers
+    @param target: An integer
+    @return: An integer
+    """
+
+    def __init__(self):
+        self.count = 0
+
+    def backPackV(self, nums, target):
+        if not nums or not target:
+            return 0
+
+        n = len(nums)
+        m = target
+
+        dp = [[0 for j in range(m+1)] for i in range(2)]
+        for i in range(2):
+            dp[i][0] = 1
+
+        for i in range(n):
+            for j in range(m+1):
+                if i == 0:
+                    if j == nums[0]:
+                        dp[0][j] = 1
+                else:
+                    dp[i % 2][j] = dp[(i-1) % 2][j]
+                    if j >= nums[i]:
+                        dp[i % 2][j] += dp[(i-1) % 2][j-nums[i]]
+
+        print("dp:", dp)
+        return max(dp[0][-1], dp[1][-1])
