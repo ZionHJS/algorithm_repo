a3648b55d96624d391ef940a10f03e0dda8973f5
ZionHJS <hjszion@gmail.com>
2020-02-29 12:49:03 -0800

    update

---
+++ 1on1/new_course/test.py
@@ -0,0 +1,13 @@
+chars = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
+chars = chars.split(" ")
+print("chars:", chars)
+print("chars_idx:", chars.index("i"))
+
+chars = "abcdefghijklmnopqrstuvwxyz"
+print("position_b:", chars.find("b"))
+
+print("1e9:", 1e9)
+
+bulls = 0
+res = "".join(bulls)
+print("join:", res)
