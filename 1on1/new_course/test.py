3f1e2d5eb53051f5c3b2eb144b48bab359077f3c
ZionHJS <hjszion@gmail.com>
2020-03-05 17:26:50 -0800

    update

--- 1on1/new_course/test.py
+++ 1on1/new_course/test.py
@@ -1,32 +1,7 @@
-A = [34, 23, 1, 24, 75, 33, 1, 34, 24, 8, 54, 8]
-A.sort()
-print("A1:", A)
-A = set(A)
-print("A2:", A)
-
-
-class Solution:
-    def twoSumLessThanK(self, A: List[int], K: int) -> int:
-        n = len(A)
-        if n == 1:
-            return -1
-
-        A.sort()  # initialize the list
-        res = []
-        i, j = 0, n-1  # two pointer i and j from the left and right
-        while i+1 <= j:
-            cur_sum = A[i] + A[j]
-            j -= 1 if cur_sum >= K else res.append(cur_sum) i += 1
-
-        return max(res) if res else return -1
-
-
-print("res:", 1//2)
-
-word = "thisisword"
-word[1] = "o"
-print("word:", word)
-
-
-s = "Let's take LeetCode contest"
-print("len_s:", len(s))
+A = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print("pop:", A.pop(1))
+print("new_A:", A)
+A.append('saddsad')
+print("new_A:", A)
+A.remove('saddsad')
+print("new_removed:", A)
