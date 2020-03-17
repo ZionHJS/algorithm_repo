7d2ed0e8a76dda19e765890a9ee83f03dcc6d268
ZionHJS <hjszion@gmail.com>
2020-03-16 22:15:52 -0700

    update

---
+++ 1on1/new_course/test.py
@@ -0,0 +1,12 @@
+preorder = [3, 9, 20, 15, 7]
+print("preorder_last:", preorder[-3:-1])
+
+
+pop = preorder.pop(0)
+print("pop:", pop)
+print("preorder:", preorder)
+
+
+inorder = [9, 3, 15, 20, 7]
+postorder = [9, 15, 7, 20, 3]
+print("postorder:", postorder[-4:-1])
