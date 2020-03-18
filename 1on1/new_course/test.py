b1fc76e9b93a39120a1d6eab38789f802ac0e8b4
ZionHJS <hjszion@gmail.com>
2020-03-18 13:35:03 -0700

    update

--- 1on1/new_course/test.py
+++ 1on1/new_course/test.py
@@ -10,3 +10,9 @@ print("preorder:", preorder)
 inorder = [9, 3, 15, 20, 7]
 postorder = [9, 15, 7, 20, 3]
 print("postorder:", postorder[-4:-1])
+
+inorder = [9, 3, 15, 20, 7]
+str_inorder = str(inorder)
+print("str_inorder:", str_inorder)
+list_inorder = list(str_inorder)
+print("list_inorder:", list_inorder)
