913d78cf73c2ac15c60ed65bb262e14773f227fa
ZionHJS <hjszion@gmail.com>
2020-03-28 12:49:03 -0700

    update

--- 1on1/new_course/403-frogjump.py
+++ 1on1/new_course/403-frogjump.py
@@ -28,3 +28,19 @@ class Solution:
                         break
 
         return False
+
+
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        memo, stones, target = {}, set(stones), stones[-1]
+        # dfs
+
+        def dfs(unit, last):
+            if unit == target:
+                return True
+            if (unit, last) not in memo:
+                memo[(unit, last)] = any(dfs(unit + move, move)
+                                         for move in (last - 1, last, last + 1) if move and unit + move in stones)
+            return memo[(unit, last)]
+
+        return dfs(1, 1) if 1 in stones else False
