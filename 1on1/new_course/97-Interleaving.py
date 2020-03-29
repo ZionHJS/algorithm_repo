63891cdd7a986542825afa4e68ae8843339cf73d
ZionHJS <hjszion@gmail.com>
2020-03-29 11:06:49 -0700

    update

--- 1on1/new_course/97-Interleaving.py
+++ 1on1/new_course/97-Interleaving.py
@@ -10,11 +10,12 @@ class Solution:
             visited.add((temp_s1, cur_s3))
             if len(temp_s1) == 0:
                 if cur_s3 == s2:
-                    # return True
                     self.boolean = True
                 return
 
             for i in range(idx, len(cur_s3)):
+                if self.boolean or len(cur_s3[i:]) < len(temp_s1):
+                    break
                 if temp_s1[0] == cur_s3[i]:
                     if (temp_s1[1:], cur_s3[:i]+cur_s3[i+1:]) not in visited:
                         dfs(temp_s1[1:], i, cur_s3[:i]+cur_s3[i+1:])
