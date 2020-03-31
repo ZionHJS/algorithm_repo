4f13aca4de151aec769783dd3cdfaaad59712fff
ZionHJS <hjszion@gmail.com>
2020-03-30 17:47:22 -0700

    update

--- 1on1/new_course/835-image-overlap.py
+++ 1on1/new_course/835-image-overlap.py
@@ -10,19 +10,26 @@ class Solution:
                 str_B += str(B[i][j])
         print("str_A:", str_A, "str_B:", str_B)
 
-        for i in range(n):
-            for j in range(n):
-                tmp_ans_A, tmp_ans_B = 0, 0
-                for k in range((i+1)*n):
-                    #print("k:", k)
-                    if k % n <= j:
-                        #print("k%n:", k%n, "j:", j)
-                        if str_A[-k-1] == str_B[k] == 1:
-                            print("inhere!")
-                            tmp_ans_A += 1
-                        if str_B[-k-1] == str_A[k] == 1:
-                            tmp_ans_B += 1
-                print("tmp_ans_A:", tmp_ans_A, "tmp_ans_B:", tmp_ans_B)
-                ans = max(ans, tmp_ans_A, tmp_ans_B)
+        for i in range(1, 2*len(str_A)+1):
+            temp_ans = 0
+            if i <= len(str_A):
+                compare_A, compare_B = str_A[-i:], str_B[:i]  # from 1 ~ n
+                remainder = i % n
+            else:
+                # from n+1 ~ 2n
+                compare_A, compare_B = str_A[:-
+                                             (i-len(str_A))], str_B[i-len(str_A):]
+                remainder = (2*len(str_A)-i) % n
+
+            print("i:", i, "remainder:", remainder)
+            print("com_A:", compare_A, "com_B:", compare_B)
+            for j in range(len(compare_A)):
+                if remainder == 0 or (j % 3 <= remainder and j % 3 > 0):
+                    print("inhere!")
+                    if compare_A[j] == compare_B[j] == "1":
+                        temp_ans += 1
+                        print("temp_ans+:", temp_ans)
+
+            ans = max(ans, temp_ans)
 
         return ans
