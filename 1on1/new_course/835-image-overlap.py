b34033875ab6b5df67bbde82fd28a1daa63abc31
ZionHJS <hjszion@gmail.com>
2020-03-30 18:55:01 -0700

    update

--- 1on1/new_course/835-image-overlap.py
+++ 1on1/new_course/835-image-overlap.py
@@ -1,35 +1,26 @@
-class Solution:
-    def largestOverlap(self, A: List[List[int]], B: List[List[int]]) -> int:
-        n = len(A)
-        ans = 0
-        str_A, str_B = "", ""
+def largestOverlap(self, A: List[List[int]], B: List[List[int]]) -> int:
+    self.n, self.ans, str_A, str_B = len(A), 0, "", ""
+           # make List_A and List_B to a continuously string first , then we compare from 1d-emensional rather than 2d-mensional
+    for i in range(self.n):
+        for j in range(self.n):
+            str_A += str(A[i][j])
+            str_B += str(B[i][j])
 
-        for i in range(n):
-            for j in range(n):
-                str_A += str(A[i][j])
-                str_B += str(B[i][j])
-        print("str_A:", str_A, "str_B:", str_B)
-
-        for i in range(1, 2*len(str_A)+1):
+    # our compare def to compare two string when they are overlaped
+    def compare(str_1, str_2):
+        for i in range(1, len(str_A)+1):
             temp_ans = 0
-            if i <= len(str_A):
-                compare_A, compare_B = str_A[-i:], str_B[:i]  # from 1 ~ n
-                remainder = i % n
-            else:
-                # from n+1 ~ 2n
-                compare_A, compare_B = str_A[:-
-                                             (i-len(str_A))], str_B[i-len(str_A):]
-                remainder = (2*len(str_A)-i) % n
+            compare_1, compare_2 = str_1[-i:], str_2[:i]  # from 1 ~ n
+            remainder = i % self.n
 
-            print("i:", i, "remainder:", remainder)
-            print("com_A:", compare_A, "com_B:", compare_B)
-            for j in range(len(compare_A)):
-                if remainder == 0 or (j % 3 <= remainder and j % 3 > 0):
-                    print("inhere!")
-                    if compare_A[j] == compare_B[j] == "1":
-                        temp_ans += 1
-                        print("temp_ans+:", temp_ans)
+               # count
+               for j in range(len(compare_1)):
+                    if remainder == 0 or ((j+1) % self.n <= remainder and (j+1) % self.n > 0):
+                        if compare_1[j] == compare_2[j] == "1":
+                            temp_ans += 1
+                self.ans = max(self.ans, temp_ans)  # get the max count
 
-            ans = max(ans, temp_ans)
+    compare(str_A, str_B)
+    compare(str_B, str_A)
 
-        return ans
+    return self.ans
