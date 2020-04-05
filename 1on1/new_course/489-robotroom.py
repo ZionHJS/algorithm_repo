0e89b5da9e97052fb75230bef9f49234d80a9d8d
ZionHJS <hjszion@gmail.com>
2020-04-05 12:29:28 -0700

    update

--- 1on1/new_course/489-robotroom.py
+++ 1on1/new_course/489-robotroom.py
@@ -29,7 +29,6 @@
 #        Clean the current cell.
 #        :rtype void
 #        """
-import random
 
 
 class Solution:
@@ -38,34 +37,48 @@ class Solution:
         self.cur_x, self.cur_y = 0, 0
         self.map = {}
         self.x_, self.y_ = [-1, 0, 1, 0], [0, 1, 0, -1]
+        self.opp = {0: 2, 1: 3, 2: 0, 3: 1}
 
     def cleanRoom(self, robot):
+        cur_cnt = self.turn_cnt
         self.map[(self.cur_x, self.cur_y)] = 1
         robot.clean()
+        moved = 0
         while robot.move():
-            self.cur_x = self.cur_x + self.x_[self.turn_cnt]
-            self.cur_y = self.cur_y + self.y_[self.turn_cnt]
+            moved += 1
+            self.cur_x = self.cur_x + self.x_[cur_cnt]
+            self.cur_y = self.cur_y + self.y_[cur_cnt]
             self.map[(self.cur_x, self.cur_y)] = 1
             robot.clean()
-        self.nxt_x = self.cur_x + self.x_[self.turn_cnt]
-        self.nxt_y = self.cur_y + self.y_[self.turn_cnt]
-        self.map[(self.nxt_x, self.nxt_y)] = 0
+        self.blk_x = self.cur_x + self.x_[cur_cnt]
+        self.blk_y = self.cur_y + self.y_[cur_cnt]
+        self.map[(self.blk_x, self.blk_y)] = 0
 
-        # try turn left or right
-
-        rnd = random.randint(0, 1)
-        if rnd:
-            robot.turnLeft()
-            self.turn_cnt -= 1
-            if self.turn_cnt < 0:
-                self.turn_cnt = 3
-            self.cleanRoom(robot)
+        # try all nxt_directions(x4)  dfs()
+        for i in range(4):
+            if i != cur_cnt:
+                nxt_x, nxt_y = self.cur_x+self.x_[i], self.cur_y+self.y_[i]
+                if (nxt_x, nxt_y) not in self.map:
+                    left, right = 0, 0
+                    if i - self.turn_cnt > 0:
+                        for k in range(i-self.turn_cnt):
+                            robot.turnRight()
+                    else:
+                        for k in range(self.turn_cnt-i):
+                            robot.turnLeft()
+                    self.turn_cnt = i
+                    self.cleanRoom(robot)
+        # robot 还需要开回到dfs()启动前的状态    通过moved和i 两个属性来操作
+        back_cnt = self.opp[cur_cnt]
+        if back_cnt - self.turn_cnt > 0:
+            for k in range(back_cnt - self.turn_cnt):
+                robot.turnRight()
         else:
+            for k in range(self.turn_cnt - back_cnt):
+                robot.turnLeft()
+        while moved:
+            moved -= 1
+            robot.move()
+        for k in range(2):
             robot.turnRight()
-            self.turn_cnt += 1
-            if self.turn_cnt > 3:
-                self.turn_cnt = 0
-            self.cleanRoom(robot)
-
-    def nxt_turn(self):
-        rnd = random.randint(0, 1)
+        self.turn_cnt = cur_cnt
