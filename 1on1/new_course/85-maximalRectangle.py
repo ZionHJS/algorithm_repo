b273447541c79f47a9faf3ad56eb88c8c6d3a651
ZionHJS <hjszion@gmail.com>
2020-04-09 23:28:40 -0700

    update

--- 1on1/new_course/85-maximalRectangle.py
+++ 1on1/new_course/85-maximalRectangle.py
@@ -28,3 +28,25 @@ class Solution:
                 res = max(res, times*(y2-y1+1))
 
         return res
+
+        class Solution:
+
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        if not matrix or not matrix[0]:
+            return 0
+        m, n = len(matrix[0]), len(matrix)
+        height = [0] * (m + 1)  # 这个就是所谓的桶排序??? BucketSort
+        ans = 0
+
+        for row in matrix:  # O(n*(2*m))
+            for i in range(m):
+                height[i] = height[i] + 1 if row[i] == '1' else 0
+                #print("height:", height)
+            stack = [-1]
+            for i in range(m + 1):
+                while height[i] < height[stack[-1]]:
+                    h = height[stack.pop()]
+                    w = i - 1 - stack[-1]
+                    ans = max(ans, h * w)
+                stack.append(i)
+        return ans
