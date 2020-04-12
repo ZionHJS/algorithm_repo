bed21b1e5e70581e38c65a56772e327ac629281b
ZionHJS <hjszion@gmail.com>
2020-04-12 00:37:09 -0700

    update

--- 1on1/new_course/212WordSearch.py
+++ 1on1/new_course/212WordSearch.py
@@ -93,20 +93,20 @@ class Solution:
         m, n = len(B[0]), len(B)
         if m == n == 1:
             return B[0][0] if B[0][0] in words else []
-        graph, memo = collections.defaultdict(lambda: []), {}
+        graph, self.memo = collections.defaultdict(lambda: []), {}
         for word in words:
             graph[word[0]].append(word)
         m, n = len(B[0]), len(B)
-        ans = set()
+        self.ans = set()
         x_, y_ = [-1, 1, 0, 0], [0, 0, -1, 1]
         for i in range(n):
             for j in range(m):
                 if B[i][j] in graph:
                     targets = graph[B[i][j]]
                     visited = set()
-                    ans = set.union(ans, self.dfs(
+                    self.ans = set.union(self.ans, self.dfs(
                         m, n, i, j, targets, 0, B, x_, y_, visited))
-        return ans
+        return self.ans
 
     def dfs(self, m, n, y, x, targets, idx, B, x_, y_, visited):
         if not targets:
@@ -118,13 +118,14 @@ class Solution:
             if self.is_valid(nxt_y, nxt_x, m, n) and (nxt_y, nxt_x) not in visited:
                 nxt_targets = []
                 for target in targets:
-                    if len(target) == 1:
-                        tmp_ans.add(target)
-                    if idx < len(target)-1 and B[nxt_y][nxt_x] == target[idx+1]:
-                        if idx+1 == len(target)-1:
+                    if target not in self.ans:
+                        if len(target) == 1:
                             tmp_ans.add(target)
-                        else:
-                            nxt_targets.append(target)
+                        if idx < len(target)-1 and B[nxt_y][nxt_x] == target[idx+1]:
+                            if idx+1 == len(target)-1:
+                                tmp_ans.add(target)
+                            else:
+                                nxt_targets.append(target)
                 if nxt_targets:
                     tmp_ans = set.union(tmp_ans, self.dfs(
                         m, n, nxt_y, nxt_x, nxt_targets, idx+1, B, x_, y_, visited))
