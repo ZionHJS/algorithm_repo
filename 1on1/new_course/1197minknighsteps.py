58b46e79316a5fbc9e963bb56483a8065a1dfeb5
ZionHJS <hjszion@gmail.com>
2020-04-20 15:53:09 -0700

    update

--- 1on1/new_course/1197minknighsteps.py
+++ 1on1/new_course/1197minknighsteps.py
@@ -31,3 +31,30 @@ class Solution:
                     if (nxs <= xs or nys <= ys) and (nxt_x, nxt_y) not in visited:
                         q.put((nxt_x, nxt_y))
                         visited.add((nxt_x, nxt_y))
+
+
+class Solution:
+    def minKnightMoves(self, x: int, y: int) -> int:
+        if x == 0 and y == 0:
+            return 0
+        # bfs()
+        directions = [(-2, -1), (-1, -2), (1, -2), (2, -1),
+                      (2, 1), (1, 2), (-1, 2), (-2, -1)]
+        cur = set()
+        visited = set()
+        steps = -1
+        xs, ys = abs(x), abs(y)  # orthogonal symmetry
+        cur.add((0, 0))
+        while cur:
+            nxt_level = set()
+            steps += 1
+            for tx, ty in cur:
+                for dx, dy in directions:
+                    nxt_x, nxt_y = tx+dx, ty+dy
+                    if (nxt_x, nxt_y) == (xs, ys):
+                        return steps+1
+                    # if (nxt_x, nxt_y) not in visited and abs(nxt_x)+abs(nxt_y)<=300 and nxt_x>=-4 and nxt_y>=-4:
+                    if (nxt_x, nxt_y) not in visited and abs(nxt_x)+abs(nxt_y) <= 300 and (nxt_x >= -1 and nxt_y >= -1):
+                        visited.add((nxt_x, nxt_y))
+                        nxt_level.add((nxt_x, nxt_y))
+            cur = nxt_level
