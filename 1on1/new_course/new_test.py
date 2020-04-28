b514f2fa9e13acdf0085ee37a2fe853d4f4888e3
ZionHJS <hjszion@gmail.com>
2020-04-28 14:49:28 -0700

    update

--- 1on1/new_course/new_test.py
+++ 1on1/new_course/new_test.py
@@ -6,3 +6,12 @@ print("key:", key)
 a = 1 & 1 == 1
 b = 0 | 0 == 1
 print("a-TorF:", a, "b-TorF:", b)
+
+a = int(0b110)
+b = int(0b011)
+print("a:", a, "b:", b)
+h = a | b
+c = int(0b111)
+q = a & b
+d = int(0b010)
+print("h:", h, "q:", q, "c:", c, "q:", q)
