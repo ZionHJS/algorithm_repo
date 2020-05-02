934465478b6b729ddae3bad95f79ef638a4a55bc
ZionHJS <hjszion@gmail.com>
2020-05-02 14:14:30 -0700

    update

--- 1on1/new_course/1096brac.py
+++ 1on1/new_course/1096brac.py
@@ -9,89 +9,88 @@ class Solution:
         cur = []
         switch = False
         for i in range(len(E)):
-            if E[i] == "{":  # in
-                if switch:
+            if E[i] == "{":
+                if switch:  # 完全正确
                     prev_pro = pros.pop()
                     product = ["".join(c) for c in itertools.product(
                         prev_pro, [chars])] if chars else prev_pro
                     pros.append(product)
-                    # stack.append(cur)
-                    print("{-switch:", "stack:", stack, "pros:", pros)
+                    #print("{-switch:", "stack:", stack, "pros:", pros)
                     cur = []
                     chars = ""
                     switch = False
-                else:
+                else:  # 完全正确
                     stack.append(cur)
                     pros.append([chars])
-                    print("stack:", stack, "pros:", pros)
+                    #print("stack:", stack, "pros:", pros)
                     cur = []
                     chars = ""
             elif E[i] != "}" and E[i] != ",":
                 chars += E[i]
-                # if i == len(E)-1:
-                #     if switch:
-                #         prev_pro = pros.pop()
-                #         product = ["".join(c) for c in itertools.product(prev_pro, [chars])] if chars else prev_pro
-                #         cur += product
-                #     else:
-                #         cur.append(chars)
-                print("change chars:", chars)
+                if i == len(E)-1:
+                    if switch:
+                        prev_pro = pros.pop()
+                        product = ["".join(c) for c in itertools.product(
+                            prev_pro, [chars])] if chars else prev_pro
+                        cur += product
+                    else:
+                        cur.append(chars)
+                #print("change chars:", chars)
             elif E[i] == ",":
                 if switch:
                     prev_pro = pros.pop()
                     product = ["".join(c) for c in itertools.product(
                         prev_pro, [chars])] if chars else prev_pro
-                    #cur += product
-                    cur = stack.pop() + cur + product
-                    print(",-switch-off-cur:", cur)
+                    cur = stack.pop() + product
+                    #print(",-switch-off-cur:", cur)
                     switch = False
                 else:
                     if chars:
                         cur.append(chars)
                 chars = ""
             elif E[i] == "}":  # out
+                if switch:
+                    prev_pro = pros.pop()
+                    product = ["".join(c) for c in itertools.product(
+                        prev_pro, [chars])] if prev_pro else cur
                 if chars:
                     cur.append(chars)
-                print("chars:", chars, "cur:", cur)
+                #print("chars:", chars, "cur:", cur)
                 prev_pro = pros.pop()
                 product = ["".join(c) for c in itertools.product(
                     prev_pro, cur)] if prev_pro else cur
                 print("product:", product)
+                if switch:
+                    cur = stack.pop()+product
+                    prev_pro = pros.pop()
+                    product = ["".join(c) for c in itertools.product(
+                        prev_pro, cur)] if prev_pro else cur
+                    print("break-swtich:", "product:", product)
+                    cur = product
+                    switch = False
+                    chars = ""
+                else:
+                    cur = product
+                    chars = ""
 
                 if i < len(E)-1:
-                    if E[i+1] != "," and E[i+1] != "}":
-                        if switch:
-                            cur = stack.pop()+product
-                            prev_pro = pros.pop()
-                            print("switch!", "cur:", cur,
-                                  "prev_pro:", prev_pro)
-                            product = ["".join(c) for c in itertools.product(
-                                prev_pro, cur)] if prev_pro else cur
-                            pros.append(product)
-                        else:
-                            pros.append(product)
-                            print("switch!", "pros:", pros)
+                    if E[i+1] != "," and E[i+1] != "}":  # if next-switch
+                        print("switch!", "cur:", cur)
+                        pros.append(cur)
                         switch = True
                         chars = ""
                         cur = []
-                        #cur = stack.pop()
-                    else:
-                        cur = stack.pop()+product
+                    else:  # if next-not-swtich
+                        cur = stack.pop()+cur
                         chars = ""
-                        print("break-cur:", cur)
-                # else:
-                #     cur=stack.pop()+product
-                #     print("the last", "cur:", cur, "stack:", stack, "pros:", pros)
 
         print("stack:", stack, "pros:", pros, "cur:", cur, "chars:", chars)
-        if chars:
-            cur.append(chars)
-        while pros:
-            prev_pro = pros.pop()
-            product = ["".join(c) for c in itertools.product(
-                prev_pro, cur)] if prev_pro else cur
-            cur = stack.pop() + product
+        #if chars: cur.append(chars)
+        # while pros:
+        #     prev_pro = pros.pop()
+        #     product = ["".join(c) for c in itertools.product(prev_pro, cur)] if prev_pro else cur
+        #     cur = stack.pop() + product
         res = list(set(cur))
-        # res.sort()
+        res.sort()
         #print("stack:", stack, "pros:", pros, "cur:", cur, "chars:", chars)
         return res
