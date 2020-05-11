a7f3f4b5777a67457cbca19db018db844abbb3e6
ZionHJS <hjszion@gmail.com>
2020-05-11 14:18:31 -0700

    update

--- 1on1/new_course/325-maxiumsubarray.py
+++ 1on1/new_course/325-maxiumsubarray.py
@@ -4,10 +4,11 @@ class Solution:
         prefix = [0 for i in range(len(nums)+1)]
         for i in range(1, len(prefix)):
             prefix[i] = prefix[i-1] + nums[i-1]
-
+        print("prefix:", prefix)
         for i in range(len(nums))[::-1]:
             for j in range(1, len(prefix)-i):
-                if prefix[j+i-1] - prefix[j-1] == k:
+                #print("i:", i, "j:", j)
+                if prefix[j+i] - prefix[j-1] == k:
                     return i
 
         return 0
