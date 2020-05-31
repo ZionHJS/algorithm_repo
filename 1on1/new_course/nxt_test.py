3d5fe14a24b4562086ab6f058b72605b9b6635d4
ZionHJS <hjszion@gmail.com>
2020-05-31 14:43:28 -0700

    update

--- 1on1/new_course/nxt_test.py
+++ 1on1/new_course/nxt_test.py
@@ -4,3 +4,10 @@ idx = bisect.bisect_left(a, "f")
 print("idx:", idx)
 idx2 = bisect.bisect_left(a[:idx], "f")
 print("idx2:", idx2)
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+s = set()
+ta = tuple(a)
+print("ta:", ta)
+s.add(ta)
+print("s:", s)
