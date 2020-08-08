5c19934cdcb8f62bd7219af850ec849bc776a667
ZionHJS <hjszion@gmail.com>
2020-08-07 17:31:40 -0700

    update

--- 1on1/new_course/nxt_test.py
+++ 1on1/new_course/nxt_test.py
@@ -266,3 +266,10 @@ if str.startswith(""):
 
 rg = range(10)
 print("rg:", rg)
+
+set1 = set({1,2,3})
+set2 = set({3,4,5})
+print("set1:", set1)
+print("set2:", set2)
+set1 = set1.union(set2)
+print("final set1:", set1)
