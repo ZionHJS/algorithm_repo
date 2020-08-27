f982e2a42837fdbe1553bfd3f49b26182b859d95
ZionHJS <hjszion@gmail.com>
2020-08-26 22:12:56 -0700

    update

--- 1on1/new_course/empty.py
+++ 1on1/new_course/empty.py
@@ -3,3 +3,7 @@ str = "$50 each. $100, $999"
 #res = re.sub(r"[$0-9]", str.lower().split)
 print("find:", str.find("$"))
 print("split:", str.split("$"))
+
+lst = [[1, 2], [3, 4], [5, 6]]
+for u, v in lst[::-1]:
+    print("u:", u, "v:", v)
