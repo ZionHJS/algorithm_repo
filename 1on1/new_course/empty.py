6bfd0875a0ea04a3cbaaeb3e6f26c2197d4a4beb
ZionHJS <hjszion@gmail.com>
2020-09-02 23:41:40 -0700

    update

--- 1on1/new_course/empty.py
+++ 1on1/new_course/empty.py
@@ -13,3 +13,21 @@ for u, v in lst[::-1]:
 
 for i in range(10, -1, -1):
     print("i:", i)
+
+num = "0010"
+print("int:", int(num))
+
+print("or:", 10 or 1)
+
+print("test:", []+[[2, 3], [4, 5]])
+
+s = set([1, 2, 3, 4, 5])
+print("set:", s)
+
+s = set()
+s.add(1)
+s.add(3)
+print("s:", s)
+
+num = -10
+print("str:", str(num))
